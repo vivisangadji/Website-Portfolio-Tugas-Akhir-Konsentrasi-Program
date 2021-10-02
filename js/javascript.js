99c648a346c5c320f65b7a9dd58cbe303988854d
vivisangadji06 <vivisangdji06@gmail.com>
2021-10-01 20:34:11 -0700

    Mengupload project tugas akhir

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+const txtElement = ['Mahasiswi', 'Freelancer', 'Web Developer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function mengetik(){
+
+    if(count == txtElement.length){
+        count = 0;
+    }
+
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-mengetik').textContent = words;
+
+    if(words.length == currentTxt.length){
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(mengetik, 500);
+
+})();
